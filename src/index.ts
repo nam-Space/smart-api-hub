0241217c31fc4538cc85a78bbce7bbc2d88b5a70
nam-Space <namyahoo2003@gmail.com>
2026-04-07 22:55:06 +0700

    add origin and credentials in cors

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,12 @@ import cors from "cors";
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  }),
+);
 
 const PORT = 3000;
 
