650875a8ea03bd28a188da7e0cb1ea44cec8905f
nam-Space <namyahoo2003@gmail.com>
2026-04-07 16:42:41 +0700

    change url port postgresql

--- src/db/knex.ts
+++ src/db/knex.ts
@@ -4,11 +4,12 @@ import "dotenv/config";
 // Kết nối đến PostgreSQL
 export const db = knex({
   client: "pg",
-  connection: {
-    host: process.env.DB_HOST || "localhost",
+  // Nếu có DATABASE_URL (trên Render) thì dùng nó, nếu không (local) thì dùng config cũ
+  connection: process.env.DATABASE_URL || {
+    host: process.env.DB_HOST || "127.0.0.1",
     port: Number(process.env.DB_PORT) || 5432,
-    database: process.env.DB_NAME || "postgres",
     user: process.env.DB_USER || "postgres",
-    password: process.env.DB_PASSWORD || "123456", // Thay bằng pass của bạn
+    password: process.env.DB_PASSWORD || "postgres",
+    database: process.env.DB_NAME || "postgres_db",
   },
 });
