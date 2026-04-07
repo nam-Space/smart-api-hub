3d1b7d19c15ca53ea25557fd1675b743c4d50c88
nam-Space <namyahoo2003@gmail.com>
2026-04-07 23:16:25 +0700

    fix db url connectionString

--- src/db/knex.ts
+++ src/db/knex.ts
@@ -4,8 +4,9 @@ import "dotenv/config";
 // Kết nối đến PostgreSQL
 export const db = knex({
   client: "pg",
-  // Nếu có DATABASE_URL (trên Render) thì dùng nó, nếu không (local) thì dùng config cũ
-  connection: process.env.DATABASE_URL || {
+  connection: {
+    // Nếu có DATABASE_URL (Render) thì dùng nó, không thì dùng các biến lẻ (Local)
+    connectionString: process.env.DATABASE_URL,
     host: process.env.DB_HOST || "127.0.0.1",
     port: Number(process.env.DB_PORT) || 5432,
     user: process.env.DB_USER || "postgres",
--- src/index.ts
+++ src/index.ts
@@ -11,12 +11,7 @@ import cors from "cors";
 
 const app = express();
 app.use(express.json());
-app.use(
-  cors({
-    origin: true,
-    credentials: true,
-  }),
-);
+app.use(cors());
 
 const PORT = 3000;
 
